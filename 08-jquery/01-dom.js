9714e4bea3983fabd0589165d201f6a3efd9059d
Sri Lakshmi Polavarapu <srilakshmipolavarapu@Sris-MacBook-Air.local>
2024-02-29 21:58:20 -0800

    Lab-8-jQuery

--- 08-jquery/01-dom.js
+++ 08-jquery/01-dom.js
@@ -1,48 +1,43 @@
-// Exercise #1:
-// When the user clicks the 'copy' button, copy the user input to the output area
+//Ex-1
 
-// Option #1
-// Fetch JavaScript objects representing specific elements in the DOM
-let userInput1 = document.querySelector('#userInput1');
-let copy = document.querySelector('#copy');
-let output1 = document.querySelector('#output1');
+let userInput = document.querySelector("#userInput1");
+let copy = document.querySelector("#copy");
+let output = document.querySelector("#output");
 
-// Add an event listener on the target element
-copy.addEventListener('click', handleClick);
+//event listener
+copy.addEventListener("click", handleClick);
 
-// Callback function to handle event
+//this is function callback
 function handleClick(event) {
-  console.log('click event', event);
-  output1.textContent = userInput1.value;
+  console.log("click event", handleClick);
+  output.textContent = userInput.value;
 }
 
-// Option #2
-// document.getElementById('copy').onclick = () => {
-//   let userInput = document.getElementById('userInput1');
-//   let output = document.getElementById('output');
-
+//other ways
+//1.
+//document.getElementById('copy').onclick=()=>{
+//   let userInput=document.getElementById('userInput1');
+//   let output=document.getElementById('output')
 //   output.textContent = userInput.value;
-// };
+//};
 
-// Option #3
-// document.getElementById('copy').onclick = () => {
-//   document.getElementById('output').textContent = document.getElementById(
-//     'userInput1'
-//   ).value;
-// };
+//2.
+//document.getElementById('copy').onclick=()=>{
+//document.getElementById('output').textContent= document.getElementById('userInput1').value;
+//};
 
-// Exercise #2:
-// When the user enters input text, copy the user input to the output area
+//Ex-2
+let userInput2 = document.querySelector("#userInput2");
+//add event listener
+userInput2.addEventListener("input", handleInput);
 
-// Fetch JavaScript objects representing specific elements in the DOM
-let userInput2 = document.querySelector('#userInput2');
-let output2 = document.querySelector('#output2');
+let section = document.querySelector("#inputEventExample");
+let element = document.createElement("div");
+element.setAttribute("class", "output");
+section.append(element);
 
-// Add an event listener on the target element
-userInput2.addEventListener('input', handleInput);
-
-// Callback function to handle event
+//function call
 function handleInput(event) {
-  console.log('click event', event);
-  output2.textContent = userInput2.value;
+  console.log(userInput2.value);
+  element.textContent = userInput2.value;
 }
--- 08-jquery/02-ajax.js
+++ 08-jquery/02-ajax.js
@@ -1,32 +1,32 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+document.body.style.backgroundColor = "skyblue";
+const url = "https://anapioficeandfire.com/api/books/";
 
-const app = document.querySelector('#books');
+const app = document.querySelector("#book");
 app.style.paddingLeft = 0;
-const loading = document.querySelector('#loading');
+const loading = document.querySelector("#loader");
 
 const addBookToDOM = (item) => {
   console.log(item);
-  let element = document.createElement('div');
-  let title = document.createElement('h4');
-  let author = document.createElement('p');
-  let published = document.createElement('p');
-  let pages = document.createElement('p');
+  let element = document.createElement("div");
+  let heading = document.createElement("h3");
+  let publisher = document.createElement("p");
+  let published = document.createElement("p");
+  let pages = document.createElement("p");
 
-  element.style.display = 'flex';
-  element.style.flexDirection = 'column';
-  element.style.alignItems = 'center';
-  element.style.marginTop = '20px';
+  element.style.display = "flex";
+  element.style.flexDirection = "column";
+  element.style.alignItems = "center";
+  element.style.marginTop = "20px";
 
-  title.textContent = item.name;
-  author.textContent = `by ${item.authors[0]}`;
+  heading.textContent = item.name;
+  publisher.textContent = item.authors[0];
   published.textContent = item.released.substr(0, 4);
   pages.textContent = `${item.numberOfPages} pages`;
 
-  element.append(title);
-  element.append(author);
+  element.append(heading);
+  element.append(publisher);
   element.append(published);
   element.append(pages);
-
   app.append(element);
 };
 
@@ -34,17 +34,21 @@ const fetchData = (url) => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
+      console.log(data);
+
       data.forEach((item) => {
+        console.log(item.name, item.authors[0]);
         addBookToDOM(item);
       });
     })
     .catch((error) => {
       console.log(error);
-      let li = document.createElement('li');
-      li.textContent = `An error occured. Please try again.`;
-      app.append(li);
+      let div = document.createElement("div");
+      div.textContent = `Please try again later`;
+      app.append(div);
     })
     .finally(() => {
+      let loading = document.querySelector("#loader");
       app.removeChild(loading);
     });
 };
